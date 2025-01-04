65c26209ff49619cef2386dfe41be78108990190
Jayakrushna35 <jayakrushnapattnaik6@gmail.com>
2025-01-04 20:20:04 +0530

    file creation

--- indes.js
+++ indes.js
@@ -1,4 +1,15 @@
 const express = require("express");
+const path = require("path");
 
 
 const app = express();
+const PORT = 8000;
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
+
+
+
+app.listen(PORT, ()=> console.log("Server Started at PORT : 8000"))
+
